d99842c483b6fe0dae91ef0a9c5318667bed701b
Nibin K <nibin9797@gmail.com>
2022-02-06 14:59:50 +0530

    Initial spring boot application setup done

---
+++ src/main/java/com/project/moviedateServer/MoviedateServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.moviedateServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviedateServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/moviedateServer/MoviedateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.moviedateServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
